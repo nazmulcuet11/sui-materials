ea9fabb2c3d1ab992d7b5c626f1cd65da4e02e90
nazmulcuet11 <nazmulcuet11@gmail.com>
2021-12-09 19:13:17 +0600

    Add sliders and color views

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,45 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, world!")
-      .padding()
-  }
+    @State var game = Game()
+    @State var guess: RGB
+    
+    var body: some View {
+        VStack {
+            Color(rgbStruct: game.target)
+            Text("R: ??? G: ??? B: ????")
+                .padding()
+            Color(rgbStruct: guess)
+            Text(guess.intString())
+                .padding()
+            ColorSlider(value: $guess.red, trackColor: .red)
+            ColorSlider(value: $guess.green, trackColor: .green)
+            ColorSlider(value: $guess.blue, trackColor: .blue)
+            Button(action: {}) {
+                Text("Button")
+            }
+            
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView(guess: RGB())
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var trackColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .accentColor(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
+    }
 }
