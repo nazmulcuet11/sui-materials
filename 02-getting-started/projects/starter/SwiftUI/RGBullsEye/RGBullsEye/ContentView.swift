24bd24c46f4f3bf6b1b089071e02465f28f768f0
nazmulcuet11 <nazmulcuet11@gmail.com>
2021-12-10 11:47:03 +0600

    Set simulator device to iPhone 11 for preview `.previewDevice("iPhone 11")` to prevent soptlight cpu usage issues

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -81,6 +81,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(guess: RGB())
+            .previewDevice("iPhone 11")
     }
 }
 
